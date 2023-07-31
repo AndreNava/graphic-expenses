b2763f0180ed2ad3daf8c9ec6f56bc6937da1b87
Andre Nava <jandrenavat@gmail.com>
2023-07-31 15:59:20 -0600

    Elimando el boton editar input

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,12 +5,12 @@ import Card from "../UI/Card";
 import './ExpenseItem.css';
 
 function ExpenseItem (props){
-  const [title, setTitle] = useState(props.title); // [var, funcion a usar] = estado (leer el dato enviado en title) esto es un decontructivo
-  console.log(title);
-  
-  const clickHandler = ()=> {
-    setTitle('Updated!!');
-  }; //Es lo que hara al captar el evento click
+  //const [title, setTitle] = useState(props.title); // [var, funcion a usar] = estado (leer el dato enviado en title) esto es un decontructivo
+  //console.log(title);
+
+  //const clickHandler = ()=> {
+    //setTitle('Updated!!');
+  //}; //Es lo que hara al captar el evento click
 
     return ( 
     <li>
@@ -20,7 +20,7 @@ function ExpenseItem (props){
           <h2>{props.title}</h2>
           <div className='expense-item__price'>${props.amount}</div>
         </div> 
-        <button onClick={clickHandler}> Change Title</button>
+        {/*<button onClick={clickHandler}> Change Title</button>*/}
       </Card>
     </li> // se usa como envoltura en lugar del div
     );//usando Card como etiqueta se jala los estilos css que tenemos en ese componente 
